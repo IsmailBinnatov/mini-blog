bd8fd8cce1204a7fd7e20c5a0ee60b31a694d942
Ismll <ismll.acc@mail.ru>
2024-08-24 16:21:15 +0400

    created and migrated post model, connect in admin p.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    ''' Данные о посте '''
+    title = models.CharField('Заголовок записи', max_length=100)
+    description = models.TextField('Текст записи')
+    author = models.CharField('Автор', max_length=100)
+    created_at = models.DateField('Дата публикации', auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.title} - {self.author}'
+
+    class Meta:
+        verbose_name = 'Запись'
+        verbose_name_plural = 'Записи'
